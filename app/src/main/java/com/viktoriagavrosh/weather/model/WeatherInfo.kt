22fd920ab082415e406400db2c2fdda715b59987
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2024-03-19 20:11:18 +0300

    app created - first commit

---
+++ app/src/main/java/com/viktoriagavrosh/weather/model/WeatherInfo.kt
@@ -0,0 +1,9 @@
+package com.viktoriagavrosh.weather.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class WeatherInfo {
+    //@SerialName(value = "") val
+}
